c52b8b5608533779a643a5fd958c0ae14ccfb9ce
DeibyOd <deibyod@gmail.com>
2025-02-13 14:20:55 -0300

    Footer version

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2025. Juego de Ajedrez. Desarrollado por DeibyOd. Todos los derechos reservados.</p>
+      <p>&copy; 2025. Juego de Ajedrez. Desarrollado por DeibyOd. Todos los derechos reservados. v1.0</p>
     </footer>
   );
 };
